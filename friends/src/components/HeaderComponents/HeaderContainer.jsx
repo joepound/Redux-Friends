25c9ed53976fdf149c8560a0260b57d223dddc03
joepound <14357797+joepound@users.noreply.github.com>
2019-01-31 20:25:58 -0700

    Changed Header component text and class names (stopped using wrong app name)

--- friends/src/components/HeaderComponents/HeaderContainer.jsx
+++ friends/src/components/HeaderComponents/HeaderContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderContainer = props => {
   return (
-    <h1 className="todo-app__app-name">Todo App</h1>
+    <h1 className="friends-app__app-name">Friends</h1>
   );
 };
 
